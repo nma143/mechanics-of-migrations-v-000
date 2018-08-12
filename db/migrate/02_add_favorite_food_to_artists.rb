b861b73ae44ef36d88ba7828c75bc57ca031181b
Nikki A <nikki.apostolakis@gmail.com>
2018-08-12 15:41:19 +0000

    automatically backed up by learn

--- db/migrate/02_add_favorite_food_to_artists.rb
+++ db/migrate/02_add_favorite_food_to_artists.rb
@@ -1,3 +1,4 @@
+# add a column to the artists table called favorite_food and that it will contain a string
 class AddFavoriteFoodToArtists < ActiveRecord::Migration
   def change
     add_column :artists, :favorite_food, :string
